e68726cb00ef8ef71cd68542b95ed28e765f4f6b
Stuepp <arthurh.c@hotmail.com>
2022-09-17 20:18:45 -0300

    1-done

--- exercicios-fork/1.c
+++ exercicios-fork/1.c
@@ -5,20 +5,32 @@
 #include <sys/wait.h>
 
 int main(void){
-  pid_t f, w;
-  int status = 0;
+  pid_t f, f2, f3;
+  int letra = 65, d = 0;//abcd
 
   printf("Processo principal (PID=%u)\n", getpid());
   f = fork();
   if(f == 0){
     //do something
-    sleep(1);
-    printf("%i\n", status);
-    status++;
-    exit(0);
-  } else {
+    //sleep(1);
+    f2 = fork();
+    if(f2 == 0){//2º ou 3º
+      letra++;
+      printf("B - %c\n", letra);
+    } else{ //4º ou 3º
+      f3 = fork();
+      if(f3 == 0){
+        letra++;
+        printf("D - %c\n", letra);
+      }else{ //3º ou 2º
+        letra++;
+        printf("C - %c\n", letra);
+      }
+    }
+  } else { // pai 1º
     //do another 
-    printf("A\n");
+    printf("A - %c\n", letra);
+    waitpid(f, NULL, 0);
   }
   return 0;
 }
