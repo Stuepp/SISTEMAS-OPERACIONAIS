141eb43a37a34f8e37323ad0e205095c7f429ba4
Stuepp <arthurh.c@hotmail.com>
2022-09-21 21:38:01 -0300

    ipc-5 incompleto

---
+++ exerc-ipc-linux/ipc-5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <conio.h>
+
+int main( int argc, char *argv[ ] ){
+  int Nthreads = (int)*argv[0];
+
+  printf("%d\n", Nthreads);
+
+  return 0;
+}
